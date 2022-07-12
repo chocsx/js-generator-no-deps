ae09436e15ca26dcb3ba05c03b7a299d11be0f3a
Mateus Sobrinho <mateus.webphp@gmail.com>
2022-07-12 20:09:39 -0300

    feat(server2): finish server implementation:

--- src/server2.js
+++ src/server2.js
@@ -1,10 +1,21 @@
-//curl "localhost:3000/products?name=laptop"
+//curl -X POST "localhost:4000/cart" --data '{"id": 123}'
 
 import { createServer } from 'http'
+import { once } from 'node:events'
 
 const PORT = 4000
 async function handler(request, response) {
+  if(
+    request.method === 'POST' &&
+    request.url.includes('cart')
+  ) {
+    const data = await once(request, 'data')
+    const item = JSON.parse(data)
 
+    console.log('received', item)
+
+    return response.end(`process succeeded for ${item.id}`)
+  }
   return response.end(`hey!`)
 }
 
